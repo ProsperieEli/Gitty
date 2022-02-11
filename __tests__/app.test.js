626a35d0a080a5f2e2863d1df549cbeddb4424ff
ProsperieEli <ElijahProsperie@gmail.com>
2022-02-10 19:58:53 -0500

    finished users with passing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,8 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/utils/GitHubUser');
+
 describe('backend routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -10,5 +12,40 @@ describe('backend routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should redirect to github', async () => {
+    const req = await request(app).get('/api/v1/github/login');
+
+    expect(req.header.location).toMatch(
+      `https://github.com/login/oauth/authorize?client_id=${process.env.GH_CLIENT_ID}&scope=user&redirect_uri${process.env.GH_REDIRECT_URI}`
+    );
+  });
+  it('should login and redirect', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/login/callback/?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      // login: 'fake_user',
+      email: 'elijahprosperie@gmail.com',
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
+  });
+  it('should delete a session', async () => {
+    const res = await request
+      .agent(app)
+      .delete('/api/v1/github/sessions')
+      .send({
+        email: 'elijahprosperie@gmail.com',
+      });
+
+    expect(res.body).toEqual({
+      success: true,
+      message: 'Signed out successfully!',
+    });
   });
 });
